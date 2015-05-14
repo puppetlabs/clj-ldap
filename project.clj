9970a6e788c4dc4fe26577536e0846f057460b19
aperiodic <dlp@aperiodic.org>
2015-05-14 12:54:15 -0700

    Update dev dependencies to use dev profile.
    This is how lein 2.x specifies dev dependencies.

--- project.clj
+++ project.clj
@@ -3,14 +3,12 @@
   :url "https://github.com/puppetlabs/clj-ldap"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.unboundid/unboundid-ldapsdk "2.3.0"]]
-  :dev-dependencies [[jline "0.9.94"]
-                     [org.apache.directory.server/apacheds-all "1.5.5"]
-                     [fs "1.1.2"]
-                     [org.slf4j/slf4j-simple "1.5.6"]
-                     [lein-clojars "0.7.0"]]
+  :profiles {:dev {:dependencies [[jline "0.9.94"]
+                                  [org.apache.directory.server/apacheds-all "1.5.5"]
+                                  [fs "1.1.2"]
+                                  [org.slf4j/slf4j-simple "1.5.6"]]}}
   :aot [clj-ldap.client]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"})
-
