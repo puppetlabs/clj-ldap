013c7953ae5641e63cf19c42d2676bed3eba25ed
aperiodic <dlp@aperiodic.org>
2015-05-15 16:05:30 -0700

    Release version 0.1.1 (now with code!)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/clj-ldap "0.1.1-SNAPSHOT"
+(defproject puppetlabs/clj-ldap "0.1.1"
   :description "Clojure ldap client (Puppet Labs's fork)."
   :url "https://github.com/puppetlabs/clj-ldap"
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -7,6 +7,7 @@
                                   [org.apache.directory.server/apacheds-all "1.5.5"]
                                   [fs "1.1.2"]
                                   [org.slf4j/slf4j-simple "1.5.6"]]}}
+  :deploy-repositories [["local-test" "file:///home/dlp/lein-test"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
